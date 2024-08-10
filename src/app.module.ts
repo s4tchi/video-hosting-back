98ff42043997f7f3c4a4a22d0ff88fc976bc1701
satchi <andrey1mosckalenko@gmail.com>
2024-08-10 19:37:39 +0300

    memory about god and father

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        port: configService.get<number>('DB_PORT'),
+        name: configService.get('DB_NAME'),
+      })
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
